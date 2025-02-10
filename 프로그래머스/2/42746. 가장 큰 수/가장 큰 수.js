f95772c983fe2d5e035bbc1486bf9e493de95ec9
JEN023__ <165226466+Ganadalam@users.noreply.github.com>
2025-02-10 14:06:57 +0900

    [level 2] Title: 가장 큰 수, Time: 238.76 ms, Memory: 47.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,6 +1,6 @@
 function solution(numbers) {
     let answer = numbers.map(String)
-                        .sort((a, b) => (b + a).localeCompare(a + b))
+                        .sort((a, b) => (b + a).localeCompare(a + b)) //.localeCompare 대신 - 사용 가능
                         .join('');
     return answer[0] === '0' ? '0' : answer;
 }
