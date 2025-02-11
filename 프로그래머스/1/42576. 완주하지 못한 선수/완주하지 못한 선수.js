5c8e15d2a8b6c4b5b7450adb3c800ece4fc1ed9b
JEN023__ <165226466+Ganadalam@users.noreply.github.com>
2025-02-11 15:31:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 40.35 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -2,12 +2,10 @@ function solution(participant, completion) {
     var answer = '';
     let runnerMap = new Map();
     
-    // 참가자 이름 등장 횟수 세기
     for (let player of participant) {
         runnerMap.set(player, (runnerMap.get(player) || 0) + 1);
     }
 
-    // 완주한 선수는 횟수 감소
     for (let player of completion) {
         runnerMap.set(player, runnerMap.get(player) - 1); // 🔥 수정
     }
@@ -22,12 +20,5 @@ function solution(participant, completion) {
 
     return answer;
 }
-
-
-// 한 명만 낙오,  이름, 등장횟수 => 해시맵(키값, 씽)
-// input = 참여선수 이름들, 완주선수 이름들
-// 동명이인가능
-// 이름 몇개인지 세고, push
-// 하나이면, 걍 삭제, 둘 이상이면 --
 
 
