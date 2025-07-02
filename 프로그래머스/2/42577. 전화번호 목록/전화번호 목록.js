b0d7210950db493556de002323b654f060c92ad1
JEN023__ <165226466+Ganadalam@users.noreply.github.com>
2025-07-02 23:06:34 +0900

    [level 2] Title: 전화번호 목록, Time: 134.16 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,27 +1,18 @@
+//sort() & some() + startsWith() : O(n log n)
 function solution(phone_book) {
-    let phoneMap = new Map();
-
-    // 1. 모든 번호를 해시에 저장
-    for (let number of phone_book) {
-        phoneMap.set(number, true);
-    }
-
-    // 2. 각 번호에 대해 접두사가 해시에 있는지 확인
-    for (let number of phone_book) {
-        let prefix = "";
-        for (let digit of number) {
-            prefix += digit;
-            if (prefix !== number && phoneMap.has(prefix)) {
-                return false; // 접두사 발견
-            }
-        }
-    }
-
-    return true; // 접두사 없음
+  phone_book.sort();
+  return !phone_book.some((v, i) => phone_book[i + 1]?.startsWith(v));
 }
 
-// 접두사
-// 그러니까 
-// 1) 번호 123의 길이 알기, 짧거나 같은 길이 => 앞 글자에 포함 여부 chexk
-// 값- 쌍 => 시
-// phone_book[i]
+// // 정렬(sort & for) + 인접 비교 : O(n log n)O(n)
+// function solution(phone_book) {
+//     phone_book.sort();
+
+//     for (let i = 0; i < phone_book.length - 1; i++) {
+//         if (phone_book[i + 1].startsWith(phone_book[i])) {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
