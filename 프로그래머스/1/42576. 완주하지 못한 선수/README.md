279070c7abeb5b4734c80c799e94b455c5b77e06
JEN023__ <165226466+Ganadalam@users.noreply.github.com>
2025-07-02 22:37:31 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 95.10 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
 
 ### 성능 요약
 
-메모리: 66.3 MB, 시간: 40.35 ms
+메모리: 60.3 MB, 시간: 95.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 11일 15:31:58
+2025년 07월 02일 22:37:27
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,24 +1,10 @@
 function solution(participant, completion) {
-    var answer = '';
-    let runnerMap = new Map();
-    
-    for (let player of participant) {
-        runnerMap.set(player, (runnerMap.get(player) || 0) + 1);
+    participant.sort();
+    completion.sort();
+
+    for (let i = 0; i < completion.length; i++) {
+        if (participant[i] !== completion[i]) return participant[i];
     }
 
-    for (let player of completion) {
-        runnerMap.set(player, runnerMap.get(player) - 1); // 🔥 수정
-    }
-
-    // 0이 아닌 값 찾기 (완주하지 못한 선수)
-    for (let [key, value] of runnerMap) {
-        if (value > 0) {
-            answer = key;
-            break;
-        }
-    }
-
-    return answer;
+    return participant[participant.length - 1];
 }
-
-
